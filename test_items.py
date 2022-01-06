fec39680c38b87a5c08fa44a386d33de72892034
rf.kagirov <rf.kagirov@tensor.ru>
2022-01-06 19:15:23 +0500

    и это тоже просто добавлем

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_itemsss(browser):
+def test_nalichiya_knopki_dobaleniya_tovara(browser):
     browser.get(link)
     browser.implicitly_wait(10)
     input1 = browser.find_elements_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
