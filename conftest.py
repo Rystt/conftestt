8913de05b0fed2029025e4bc003c22c09be22371
rf.kagirov <rf.kagirov@tensor.ru>
2022-01-06 19:10:45 +0500

    добавляем

---
+++ conftest.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import pytest
+from selenium.webdriver.chrome.options import Options
+#
+#
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None, help="Язык браузера")
+
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
